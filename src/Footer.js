97ea18475a32cc3bd19871cda558d808b6a7fdd0
paulscutaru <61314329+paulscutaru@users.noreply.github.com>
2022-01-16 18:48:41 +0200

    Final

--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,7 @@ const FooterPage = () => {
     return (
         <MDBFooter className="bg-dark font-small pt-4 mt-4">
             <MDBContainer fluid className="text-center text-md-left">
-                <a className="footer-a" href="https://github.com/paulscutaru">2022 <img alt="github-logo" src="github.png"/></a>
+                <a className="footer-a" href="https://github.com/paulscutaru" target="_blank">2022 <img alt="github-logo" src="github.png"/></a>
             </MDBContainer>
         </MDBFooter>
     );
